b2c74c94b7849a5ba0179da95483669b7c705580
edmondgaks <edmondgaks@gmail.com>
2023-10-01 11:56:44 +0200

    addding the calendar component

--- src/app/(home)/calendar/page.tsx
+++ src/app/(home)/calendar/page.tsx
@@ -1,7 +1,18 @@
-const Calendar = () => {
+"use client";
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import  Calendar  from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const CalendarPage = () => {
+    const [value, onChange] = useState<Value>(new Date());
+
     return (
-        <div className="w-full h-[90vh] overflow-y-auto bg-[#F5F4F6] flex flex-col gap-y-4 px-6 py-3 overflow-x-hidden">
-            
+        <div className="w-full h-[90vh] overflow-y-auto bg-[#F5F4F6] flex flex-col gap-y-4 px-6 py-3 overflow-x-hidden"> 
             <div className="w-full flex flex-row gap-3">
                 <div className="w-[35%] flex flex-col">
                     <h1 className="font-semibold text-2xl">Calendar</h1>
@@ -23,8 +34,11 @@ const Calendar = () => {
                     </div>  
                 </div>
             </div>
+            <div className="w-full bg-white p-4">
+            <Calendar onChange={onChange} value={value} />
+            </div>
         </div>
     )
 }
 
-export default Calendar
+export default CalendarPage
